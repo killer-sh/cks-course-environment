5fc0a46ddce7db13902e75373f2535a69ccb3cbe
Tom Klingenberg <76167763+dronebeelinux@users.noreply.github.com>
2024-08-24 11:20:45 -0400

    Update install_master.sh
    changed pod-cidr

--- cluster-setup/latest/install_master.sh
+++ cluster-setup/latest/install_master.sh
@@ -181,7 +181,7 @@ systemctl enable kubelet && systemctl start kubelet
 
 ### init k8s
 rm /root/.kube/config || true
-kubeadm init --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumCPU --skip-token-print --pod-network-cidr 192.168.0.0/16
+kubeadm init --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumCPU --skip-token-print --pod-network-cidr 10.42.0.0/16
 
 mkdir -p ~/.kube
 sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
